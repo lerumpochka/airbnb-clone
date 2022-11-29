8b1838871005e983f3c4e043272e773388de8645
Nawras Aljaber <nawrasaljabr1994@gmail.com>
2022-11-29 12:50:29 +0100

    seed updated

--- database/seeders/20221129112806-admin-user.js
+++ database/seeders/20221129112806-admin-user.js
@@ -3,23 +3,20 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    
+     await queryInterface.bulkInsert('User', [{
+     name: 'John Doe',
+     email: 'john@gmail.com',
+     createdAt: new Date(),
+     updatedAt: new Date(),
+     },
+    {}], {});
+    
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    
+     await queryInterface.bulkDelete('User', null, {});
+     
   }
 };
