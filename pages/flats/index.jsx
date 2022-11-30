132f171fbd00a4ac3620c2bde08c79dba68c840d
Valeriya Rabinchuk <lerumpochka@gmail.com>
2022-11-30 19:26:46 +0100

    list all flats in /flats/index.js

--- pages/flats/index.jsx
+++ pages/flats/index.jsx
@@ -3,9 +3,12 @@ import Link from "next/link";
 import flatsController from "../../controllers/flatsController"
 
 function Flats(props) {
+  const flats = props.flats
   return (
     <div style={{ textAlign: "center" }}>
       <h1 style={{ fontSize: "50px", textAlign: "center" }}>All flats</h1>
+      
+      {flats.map(flat=><li key={flat.id}>{flat.type} in {flat.location} <br /> Foto: <br /> image {flat.imgSrc}</li>)}
       <Link href="/">Back to Home page</Link>
     </div>
   );
@@ -13,7 +16,7 @@ function Flats(props) {
 
 export async function getServerSideProps() {
   const flats = await flatsController.findAll();
-  console.log(flats)
+
   return {
       props: { flats }
   }
