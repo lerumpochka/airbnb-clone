a1ac1af43de2c4a3ba3fdad6fd3dcb37a5551064
Valeriya Rabinchuk <lerumpochka@gmail.com>
2022-12-02 09:02:43 +0100

    new flat redirect

--- pages/api/flats/index.jsx
+++ pages/api/flats/index.jsx
@@ -3,13 +3,11 @@ import flatsController from "../../../controllers/flatsController";
 export default async function handler(req, res) {
   if (req.method == "POST") {
     const data = req.body;
-    console.log('------------data---', data);
-const id = 2
-
     try {
-      await flatsController.create(data);
-      res.status(200).json({data:data})
-      // .redirect(`/flats/${id}`) //need to redirect to profile of current user
+      const flat = await flatsController.create(data);
+
+      res.status(200).redirect(`/flats/${flat.id}`) 
+      // .redirect(`/profile`)//need to redirect to profile of current user
     } catch (error) {
       res.status(400).send("error");
     }
